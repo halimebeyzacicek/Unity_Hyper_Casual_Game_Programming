10613e22522f3021df7478609c934725053feb0c
halimebeyzacicek <hbcicek25@gmail.com>
2021-09-16 04:22:38 +0300

    Create
    add_codes

---
+++ 01/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace kurs_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+            ///////////////////////////
+            int health;
+            health = 100;
+            health = health - 10;
+            //Console.WriteLine(health);
+            /////////////////////////////////
+            bool dead = health <= 0;  //koşul sağlanırsa true değerine sahip olacak
+            bool inDanger = health <= 50;//karakter tehlikede mi?
+            if(!dead)
+            {
+                if (inDanger)
+                {
+                    Console.WriteLine("Karakteriniz Tehlikede!");
+                }
+                else
+                {
+                    Console.WriteLine("Karakteriniz Güvende!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Oyunu kaybettiniz!");
+            }
+            //if (health > 50)
+            //{
+            //    Console.WriteLine("Karakteriniz Güvende!");
+            //}
+            //else if (health > 0)
+            //{
+            //    Console.WriteLine("Karakteriniz Tehlikede!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Oyunu kaybettiniz!");
+            //}
+            /////////////////////////////////
+            float height = 1.60f;
+            string name = "Halime";
+            Console.WriteLine(name + " karakterinin boyu:" + height);
+            Console.WriteLine(name + " karakterinin canı:" + health);
+            Console.ReadKey();
+            ///////////////////////////
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
